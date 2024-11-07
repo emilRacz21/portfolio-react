da219e46bd744cb276bf28b8bf60b072ea59145f
Emil Raczkiewicz <emil.raczkiewicz@gmail.com>
2024-11-07 21:12:22 +0100

    clearing the form after submitting it

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -2,8 +2,16 @@ import { useState } from "react";
 import "./Form.scss";
 
 let submitText = <p style={{ color: "white", marginTop: 0 }}>.</p>;
+
+const inputObject = {
+  name: "",
+  email: "",
+  message: "",
+};
+
 export default function Form() {
   const [text, setText] = useState(submitText);
+  const [clearInput, setClearInput] = useState(inputObject);
 
   function Submit(e) {
     e.preventDefault();
@@ -16,8 +24,7 @@ export default function Form() {
         body: formDatab,
       }
     ).then(() => {
-      console.log("sss");
-
+      setClearInput(inputObject);
       setTimeout(() => setText(submitText), 5000);
       setText(
         <p style={{ color: "green", marginTop: 0 }}>Wysłano wiadomość!</p>
@@ -30,15 +37,44 @@ export default function Form() {
 
       <p>
         <label htmlFor="name">Imię i nazwisko</label> <br />
-        <input type="text" name="Name" required />
+        <input
+          type="text"
+          value={clearInput.name}
+          onChange={(e) =>
+            setClearInput((prev) => {
+              return { ...prev, ["name"]: e.target.value };
+            })
+          }
+          name="Name"
+          required
+        />
       </p>
       <p>
         <label htmlFor="email">Adres e-mail</label> <br />
-        <input type="email" name="Email" required />
+        <input
+          type="email"
+          value={clearInput.email}
+          onChange={(e) =>
+            setClearInput((prev) => {
+              return { ...prev, ["email"]: e.target.value };
+            })
+          }
+          name="Email"
+          required
+        />
       </p>
       <p>
         <label htmlFor="message">Twoja wiadmość</label> <br />
-        <textarea name="Message" required></textarea>
+        <textarea
+          name="Message"
+          value={clearInput.message}
+          onChange={(e) =>
+            setClearInput((prev) => {
+              return { ...prev, ["message"]: e.target.value };
+            })
+          }
+          required
+        ></textarea>
       </p>
       <p>
         <button type="submit">Wyślij Teraz</button>
